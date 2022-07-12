8c6aee096237749f0d568a7e78f22d6f359e9b16
Morgan Strohl <morgan.strohl@gmail.com>
2022-07-12 12:53:37 +0200

    commit -m "création des constantes DOM"

--- front/js/product.js
+++ front/js/product.js
@@ -3,3 +3,10 @@ let url = new URL(document.location).searchParams;
 let urlID = url.get("id");
 console.log(urlID);
 
+//Création des constantes pour l'intégration dynamique dans le DOM
+const image = document.querySelector(".item__img");
+const titre = document.getElementById('title');
+const prix = document.getElementById('price');
+const description = document.getElementById('description');
+const couleur = document.getElementById('colors');
+
