750b0341bcea9c71199f3ceca448fb6d1eb19f55
Morgan Strohl <morgan.strohl@gmail.com>
2022-07-22 12:36:39 +0200

    commit -m "Ajout des commentaires"

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,3 +1,4 @@
+//Création d'un fonction nettoyant le localstorage et donnant l'orderId
 function confirmation() {
     let numeroConfirmation = document.getElementById("orderId");
     numeroConfirmation.innerText = localStorage.getItem("orderId");
